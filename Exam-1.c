f6b15b4b69196d5f90d4d6dabacda4863d340957
rorik <rorik@users.noreply.github.com>
2017-03-11 22:43:12 +0100

    Update Exam-1.c

--- Exam-1.c
+++ Exam-1.c
@@ -54,11 +54,11 @@ int main(){
 }
  
 double Apartado1(double x1, double y1, double z1, double x2, double y2, double z2){
-  double distance;
-  distance  = x2-x1 < 0 ? x1-x2 : x2-x1;
-  distance += y2-y1 < 0 ? y1-y2 : y2-y1;
-  distance += z2-z1 < 0 ? z1-z2 : z2-z1;
-  return distance;
+  double distancia;
+  distancia  = x2-x1 < 0 ? x1-x2 : x2-x1;
+  distancia += y2-y1 < 0 ? y1-y2 : y2-y1;
+  distancia += z2-z1 < 0 ? z1-z2 : z2-z1;
+  return distancia;
 }
  
 void Apartado2(long centimos){
