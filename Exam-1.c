1b81979d287ad0c0ade512e853c7c245824baebc
rorik <rorik@users.noreply.github.com>
2017-03-11 21:01:48 +0100

    Update Exam-1.c

--- Exam-1.c
+++ Exam-1.c
@@ -17,10 +17,10 @@
 *   Este programa resuelve el primer examen de la asignatura
 *   Programación de UBU.
 *
-* AUTOR : Rodrigo Díaz         START DATE : 11 Mar 2017
+* AUTOR : Rodrigo Díaz         FECHA INICIAL : 11 Mar 2017
 *
 * CAMBIOS:
-* 1.0 [11MAR2017] - Initial release
+* 1.0 [11MAR2017] - Versión inicial
 */
  
 #include <stdio.h>
@@ -36,7 +36,7 @@ int main(){
   //Primer Apartado
   printf("\n1.- * * * Distancia Manhattan * * *\nInsertar coordenadas del primer punto (x,y,z) separado por comas: ");
   scanf("%lf,%lf,%lf",&x1,&y1,&z1);
-  printf("Insertar coordenadas del segundo punto (x,y,z) separated por comas: ");
+  printf("Insertar coordenadas del segundo punto (x,y,z) separado por comas: ");
   scanf("%lf,%lf,%lf",&x2,&y2,&z2);
   printf("La distancia Manhattan es: %g.\n\n",Apartado1(x1, y1, z1, x2, y2, z2));
  
@@ -62,12 +62,12 @@ double Apartado1(double x1, double y1, double z1, double x2, double y2, double z
 }
  
 void Apartado2(long centimos){
-  printf("Equivalencia en euros: %01d %03d %03d,%02d €\n\n",(int)(centimos%100000000000/100000000),(int)(centimos%100000000/100000),(int)(centimos%100000/100),(int)(centimos%100));
+  printf("Equivalencia en euros: %01d %03d %03d,%02d \u20AC\n\n",(int)(centimos%100000000000/100000000),(int)(centimos%100000000/100000),(int)(centimos%100000/100),(int)(centimos%100));
 }
  
 long Apartado3(){
   int millones, miles, unidades, centimos;
   printf("Insertar euros formateados: ");
-  scanf("%d %d %d,%d", &milliones, &miles, &unidades, &centimos);
+  scanf("%d %d %d,%d", &millones, &miles, &unidades, &centimos);
   return (long)millones * 100000000 + miles * 100000 + unidades * 100 + centimos;
 }
